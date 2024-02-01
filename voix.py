4c78938ca5c571bf87f057e8563d1d73c7aed39f
VinZh22 <imthehellcat01@gmail.com>
2024-02-01 14:33:26 +0100

    ajouter les objects voix dans la génération

--- voix.py
+++ voix.py
@@ -13,8 +13,13 @@ class Voix :
     """
     c'est une classe virtuelle
     elle sert de classe mère pour toutes les autres voix
+    Si vous avez des changements qui s'appliquent à toutes les voix, veuillez les mettre ici
     """
     def __init__(self, vecteur_init, vecteur_rythme, l_tab, scale, output_port, tempo = 120) -> None:
+        """
+        on initialise toutes les variables
+        """
+
         self.oneTime = 60/tempo
         self.vrtm = vecteur_rythme
         self.l_tab = l_tab
@@ -23,53 +28,68 @@ class Voix :
 
         # à décider dans les classes
         self.velocity = 64
-        self.channel
-        self.program
+        # il y a aussi : self.program et self.channel
 
+        #des variables qui vont servir quand on utilisera la voix
         self.i_tab = 0 #position dans la progression d'accord
         self.boolnote= True #indique le besoin de générer une nouvelle note
-        self.note_on
-        self.t_end
-        self.root
-        self.quality
-        self.seventh
         self.len_tab = len(l_tab)
         self.v= notes.f_gamme(self.vecteur_init, scale)
         self.debut_bar = time() - 8*self.oneTime
     
+    
     def nextTime(self, t = time()):
+        """
+        Décider de la prochaine note
+        """
+
         if time() >= self.oneTime*8 + self.debut_bar: #début de mesure par la fin de la precedente
            self.changeMesure()
         
         if self.boolnote: #une nouvelle note
+
             self.t_end = self.durationNote(t)
-            self.new_note = self.newNote()
+            self.new_note = self.create_newNote()
+
             note_on = mido.Message("note_on", note = self.new_note, channel = self.channel, velocity = self.velocity)
             self.output_port.send(note_on)
             self.boolnote = False
             self.v = notes.f_note(self.v, self.new_note)
         
-        elif time() > self.t_end : #
+        elif time() > self.t_end : #arrêter la note en cours
             note_off = mido.Message("note_off", note = self.new_note, channel = self.channel, velocity = self.velocity)
             self.output_port.send(note_off)
             self.boolnote = True
 
 
     def changeMesure(self):
-        self.root, self.quality, self.seventh = self.l_tab[i_tab]
-        i_tab = (i_tab + 1)%self.len_tab
-        debut_bar += self.oneTime*8
+        """
+        Ici, comprend tout ce qui sera fait quand on change de mesure
+        Fonction qui sera appelée par toutes les classes qui héritent de Voix pour en faire leur propore version
+        """
+        self.root, self.quality, self.seventh = self.l_tab[self.i_tab]
+        self.i_tab = (self.i_tab + 1)%self.len_tab
+        self.debut_bar += self.oneTime*8
         print(self.root, self.quality, self.seventh)
     
-    def newNote(self):
+    def create_newNote(self):
         return main_droite.gen(self.v)
 
     def durationNote(self, t):
+        """
+        En principe jamais appelé, chaque classe héritée utilisera sa propre version de durationNote
+        """
         return t
 
     def choixInstrument(self):
-        instru = mido.Message("program_change", program = self.program)
+        """
+        Appelée pour chaque voix qui aura son propre instrument, et son canal (channel) de diffusion
+        """
+        instru = mido.Message("program_change", program = self.program, channel = self.channel)
         self.output_port.send(instru)
+    
+    def changeTempo(self, tempo):
+        self.oneTime = 60/tempo
 
 class VoixGauche(Voix) : 
     def __init__(self, vecteur_init, vecteur_rythme, l_tab, scale, output_port, tempo=120) -> None:
@@ -89,12 +109,12 @@ class VoixGauche(Voix) :
         self.v = self.vecteur_init
         self.v = notes.f_gamme(self.v, gammes.accord(self.root, self.quality, self.seventh))
     
-    def newNote(self):
-        return super().newNote
+    def create_newNote(self):
+        return super().create_newNote()
     
     def durationNote(self, t):
         tp_l = self.rtm[self.i_rtm]  #le nombre de temps de la note que l'on va jouer
-        i_rtm_l = (i_rtm_l + 1)%self.len_rtm
+        self.i_rtm = (self.i_rtm + 1)%self.len_rtm
         return time() + tp_l*self.oneTime
 
 class VoixDroite(Voix):
@@ -107,10 +127,11 @@ class VoixDroite(Voix):
         self.choixInstrument()
     
     def changeMesure(self):
+        super().changeMesure()
         self.v = notes.f_newtab(self.v, self.root, self.quality, self.seventh)
     
-    def newNote(self):
-        return super().newNote()
+    def create_newNote(self):
+        return super().create_newNote()
     
     def durationNote(self, t):
         tp = main_droite.gen(self.vrtm)
